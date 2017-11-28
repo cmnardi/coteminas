1adbcdae8366aeadbfe1c55c9e9e9b9d91262c04
Caio Nardi <caio@pagpouco.com>
2017-11-28 19:34:21 -0200

    Models - ativando softdelete

--- app/ProdutoImagem.php
+++ app/ProdutoImagem.php
@@ -3,10 +3,13 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ProdutoImagem extends Model
 {
     //
+    use SoftDeletes;
+
     protected $table = 'produto_imagem';
     public $timestamps = true;
 
