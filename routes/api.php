d34b0a06dee91b242bf029b982ca9e86f25f0066
Caio Nardi <caio@pagpouco.com>
2017-11-28 17:37:49 -0200

    API - definido rota, e criado um controller

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,8 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::prefix('v1')->group(function () {
+    Route::get('produto', 'ProdutoController@index');
+});
+//Route::get('/produto', 'ProdutoController@index');
