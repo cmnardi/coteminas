8cb68613e3d754c29fcf4634b8ed9e302ea7afff
Caio Nardi <caio@pagpouco.com>
2017-11-28 16:47:20 -0200

    Produto Categoria - criando model e tabela

--- app/ProdutoCategoria.php
+++ app/ProdutoCategoria.php
@@ -7,4 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class ProdutoCategoria extends Model
 {
     //
+    protected $_table = 'produtoCategoria';
+    public $timestamps = true;
+
+    const CREATED_AT = 'criado_em';
+    const UPDATED_AT = 'ultima_atualizacao';
+
 }
